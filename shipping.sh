5276a8898e591645159337aed8d2a324d15efcb1
Adii <adii...@Adiis-MacBook-Air.local>
2026-02-04 18:12:38 +0530

    learning

--- shipping.sh
+++ shipping.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 source ./common.sh
+
 app_name=shipping
 
 check_root
-app_setup
+
 java_setup
+app_setup
 systemd_setup
 
-dnf install mysql -y  &>>$LOGS_FILE
+dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+mysql -h "$MYSQL_HOST" -u root -pRoboShop@1 -e 'use cities' &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
-
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+    mysql -h "$MYSQL_HOST" -u root -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h "$MYSQL_HOST" -u root -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h "$MYSQL_HOST" -u root -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
     VALIDATE $? "Loaded data into MySQL"
 else
-    echo -e "data is already loaded ... $Y SKIPPING $N"
+    echo -e "Data already loaded ... $Y SKIPPING $N"
 fi
 
 app_restart
