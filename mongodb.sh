959eeb3a392767c7646aab7fad6e45b110e7a282
Adii <adii...@Adiis-MacBook-Air.local>
2026-02-04 13:12:18 +0530

    learning

---
+++ mongodb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root()
+cp mongo.repo /etc/yum.repos.d/mongo.repo 
+VALIDATE $? "Copying Mongo Repo"
+
+dnf install mongodb-org -y
+VALIDATE $? "Installing Mongodb Server "
+
+systemctl enable mongod
+VALIDATE $? "Enable mongod"
+
+systemctl start mongod
+VALIDATE $? "started mongod"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections" 
+
+systemctl restart mongod
+VALIDATE $? "Restarted mongodb"
+
+print_total_time()
+
---
+++ roboshop.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+SG_ID="sg-046e70dc8faeb0a65"
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z013376417K0TSZFYZ834"
+DOMAIN_NAME="ankitha.online"
+
+for instance in $@
+do 
+   INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID\
+    --instance-type t3.micro\
+    --tag-specifications \
+    "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --security-group-ids $SG_ID\
+    --query 'Instances[0].InstanceId' \
+    --output text)
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # ankitha.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.ankitha.online
+    fi  
+      
+    echo "IP Address: $IP"
+
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
+done
